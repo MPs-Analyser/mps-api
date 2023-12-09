404fb76731babf3564920d98cc9778d551cbc637
robin croft <rob@robincroft.com>
2023-12-09 10:58:23 +0000

    improve mp search

--- src/routes/searchMpsRouter.ts
+++ src/routes/searchMpsRouter.ts
@@ -10,12 +10,23 @@ searchMpsRouter.get('/', async (req: Request, res: Response) => {
   // @ts-ignore
   const result = await searchMps();
 
-  if (result && result.records) {
+  // @ts-ignore
+  const formattedResult = []
+
+  if (result && result.records && Array.isArray(result.records)) {
+    console.log("check ", result.records);
     // @ts-ignore
-    res.json(result.records);
-  } else {
-    res.json({})
+    result.records.forEach(i => {
+      // @ts-ignore          
+      formattedResult.push(
+        { name: i._fields[0], id: i._fields[1].low }
+      )
+    })
   }
+
+  // @ts-ignore
+  res.json(formattedResult)
+
 
 });
 
