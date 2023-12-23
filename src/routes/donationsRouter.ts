504b7ecb14b67650b49dd5aa36864f1ce049be30
robin croft <rob@robincroft.com>
2023-12-23 13:14:22 +0000

    merge party details

--- src/routes/donationsRouter.ts
+++ src/routes/donationsRouter.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { getDonationSummary } from "../databases/neoManager";
+import { getDonationSummary, getParties } from "../databases/neoManager";
 
 const donationsRouter = express.Router();
 
@@ -7,28 +7,42 @@ donationsRouter.get('/', async (req: Request, res: Response) => {
 
   console.log('Get Parties ', req.query);
 
+  // @ts-ignore
+  const partiesResult = await getParties();
+
+   // @ts-ignore
+   const parties = []
+
+   if (partiesResult && partiesResult.records && Array.isArray(partiesResult.records)) {
+     // @ts-ignore
+     partiesResult.records.forEach(i => {
+      parties.push({ name: i._fields[0].properties.partyName, mpsCount: i._fields[0].properties.mpsCount.low });
+     });     
+   }
 
   // @ts-ignore
   const result = await getDonationSummary();
 
-  console.log("step 2");
-  
 
   // @ts-ignore
   const formattedResult = []
 
   if (result && result.records && Array.isArray(result.records)) {
     // @ts-ignore
-    result.records.forEach(i => {
-      console.log("i > ", i);
-      
-      formattedResult.push({ 
-        partyName: i._fields[0], 
-        donationCount: i._fields[1].low, 
-        totalDonationValue: i._fields[2] 
+    result.records.forEach((item, index) => {
+
+      // @ts-ignore
+      const record = parties.find(p => p.name === item._fields[0]) 
+      const memberCount = record ? record.mpsCount : 0;
+
+      formattedResult.push({
+        partyName: item._fields[0],
+        memberCount: memberCount,
+        donationCount: item._fields[1].low,
+        totalDonationValue: item._fields[2]
       });
     });
-    
+
   }
 
   // @ts-ignore
